446032ab833a032c6eaf460f9cf111861d2165da
James <james@teamdaves.us>
2013-06-13 14:49:31 -0500

    Fix a stupid synttax error in the renderer. Re-think my templating engine choice

--- project.clj
+++ project.clj
@@ -5,6 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
+                 ;; Very tempting to replace the next line, considering the problems
+                 ;; I'm having with clostache at work. Leave it be for now, but definitely
+                 ;; think about it.
                  [de.ubercode.clostache/clostache "1.3.1"]
                  [korma "0.3.0-RC5"]
                  [org.xerial/sqlite-jdbc "3.7.2"]
