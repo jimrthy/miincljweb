daee9d1d4f4d922e6cfeb7ac79142be7c03b326c
James Gatannah <jamesgatannah@gmail.com>
2016-03-13 18:26:05 -0400

    Bump ancient dependencies

--- project.clj
+++ project.clj
@@ -3,30 +3,30 @@
   :url "http://github.com/jimrthy/miincljweb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[com.taoensso/timbre "3.4.0"]
+  :dependencies [[com.taoensso/timbre "4.3.1"]
                  ;; This depends on ring middleware.
                  ;; Which has weirdness without HttpServletRequest
-                 [compojure "1.4.0"]
+                 [compojure "1.5.0"]
                  ;; TODO: Switch to enlive
-                 [de.ubercode.clostache/clostache "1.3.1"]
+                 [de.ubercode.clostache/clostache "1.4.0"]
                  [http-kit "2.1.19"]
                  ;; Q: What was I doing with this?
                  ;; And can it go away?
                  [korma "0.4.2"]
-                 [org.clojure/clojure "1.7.0"]
-                 [org.clojure/tools.nrepl "0.2.10"]
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.nrepl "0.2.12"]
                  ;; TODO: Use a real database
                  ;; Actually, should look into options for
                  ;; handling multiple databases.
                  ;; Q: Is there any good way to do that without
                  ;; downloading a ton of needless dependencies?
                  ;; A: That really seems to be what profiles are for.
-                 [org.xerial/sqlite-jdbc "3.7.2"]
-                 [ring/ring-defaults "0.1.5"]]
+                 [org.xerial/sqlite-jdbc "3.8.11.2"]
+                 [ring/ring-defaults "0.2.0"]]
   :global-vars {*warn-on-reflection* true}
   :min-lein-version "2.0.0"
   :main miincljweb.core
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.11"]
-                                  [org.clojure/java.classpath "0.2.2"]]}}
+                                  [org.clojure/java.classpath "0.2.3"]]}}
   :repl-options {:init-ns user})
