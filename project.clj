7316b0ee5d1698ec08f42e492e78340371d07b83
James Gatannah <james.gatannah@gmail.com>
2016-03-27 17:22:53 -0500

    Switch library name
    To match recent namespace changes.
    
    Also, delete the obsolete config ns

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jimrthy/miincljweb "0.1.0-SNAPSHOT"
+(defproject com.jimrthy/cluster-web "0.1.0-SNAPSHOT"
   :description "Skeleton for hanging a website on"
   :url "http://github.com/jimrthy/miincljweb"
   :license {:name "Eclipse Public License"
